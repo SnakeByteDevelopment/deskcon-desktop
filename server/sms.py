5828f0fe47a538b9ded6d96803bb6be06bd7beb3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2014-12-07 19:17:41 +0100

    use new api in sms.py

--- server/sms.py
+++ server/sms.py
@@ -4,6 +4,7 @@ import os
 import sys
 import socket
 import configmanager
+import tls
 import json
 from OpenSSL import SSL, crypto
 from gi.repository import Gtk, GObject
@@ -46,7 +47,7 @@ class EntryWindow():
             self.errordialog.hide()
         else:
             send_sms(number,txt, self.ip, self.port, self.errordialog)
-        
+
 
     def on_cancelbutton_clicked(self, widget):
         Gtk.main_quit()
@@ -60,30 +61,12 @@ class EntryWindow():
 
 def send_sms(recver, msg, ip, port, errordialog):
     HOST, PORT = ip, int(port)
-    uuid = configmanager.uuid
-    hostname = socket.gethostname()
 
-    jsonobj = {'uuid': uuid, 'name': hostname, 
-               'type': "sms", 'data': {'number': recver, 'message': msg}}
+    data = {'number': recver, 'message': msg}
 
-    data = json.dumps(jsonobj)
-
-    # Initialize context
-    ctx = SSL.Context(SSL.TLSv1_METHOD)
-    ctx.set_options(SSL.OP_NO_SSLv2|SSL.OP_NO_SSLv3) #TLS1 and up
-    ctx.set_verify(SSL.VERIFY_PEER, verify_cb) #Demand a certificate
-    ctx.use_privatekey_file(configmanager.privatekeypath)
-    ctx.use_certificate_file(configmanager.certificatepath)
-    ctx.load_verify_locations(configmanager.cafilepath)                
-    sslclientsocket = SSL.Connection(ctx, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-
-    succ = False
     try:
-        sslclientsocket.connect((HOST, PORT))
-        sslclientsocket.sendall(data)
-        sslclientsocket.recv(2)
-        succ = True
-
+        with tls.TLSConnection(HOST, PORT) as conn:
+            conn.command('sms', data)
     except Exception as e:
         errnum = e[0]
         print "Error " + str(e[0])
@@ -95,25 +78,14 @@ def send_sms(recver, msg, ip, port, errordialog):
         errordialog.hide()
 
     finally:
-        if (succ):
-            sslclientsocket.shutdown()
-            sslclientsocket.close()
-            Gtk.main_quit()
+        Gtk.main_quit()
 
-
-
-def verify_cb(conn, cert, errnum, depth, ok):
-    # This obviously has to be updated
-    #print "er"+str(errnum)
-    #print "de"+str(depth)
-    #print "ok "+str(ok)
-    return ok
 
 def main(args):
     GObject.threads_init()
     ip = args[1]
     port = args[2]
-    if (len(args) == 4):       
+    if (len(args) == 4):
         number = args[3]
     else:
         number = ""
@@ -127,5 +99,5 @@ if __name__ == '__main__':
         print "not enough arguments given"
     else:
         main(sys.argv)
-  
+
 
