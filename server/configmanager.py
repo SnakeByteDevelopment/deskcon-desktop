7fc50af772c8d42b8f770cbc5c661ff5c76a4fc5
Switch87 <gert@pellin.be>
2017-11-19 15:59:52 +0100

    oop refactoring

--- server/configmanager.py
+++ server/configmanager.py
@@ -6,7 +6,7 @@ import authentication
 import random
 
 homedir = os.path.expanduser('~')
-default_configfile = os.path.dirname(os.path.realpath(__file__))+"/share/config.conf"
+default_configfile = os.path.dirname(os.path.realpath(__file__)) + "/share/config.conf"
 
 config = ConfigParser.ConfigParser()
 configdir = homedir + "/.deskcon/"
@@ -17,6 +17,7 @@ privatekeypath = os.path.join(keydir, "private.key")
 certificatepath = os.path.join(keydir, "server.crt")
 cafilepath = os.path.join(keydir, "cas.pem")
 
+
 def copy_default_config():
     if os.path.isdir(configdir):
         shutil.copyfile(default_configfile, configfile)
@@ -25,15 +26,18 @@ def copy_default_config():
         os.mkdir(keydir)
         shutil.copyfile(default_configfile, configfile)
 
+
 def write_file(path, data):
     with open(path, 'w') as fp:
         fp.write(data)
+
 
 def gen_and_store_keys(uuid):
     certificate, privatekey = authentication.generate_keypair(uuid)
     write_file(privatekeypath, privatekey)
     write_file(certificatepath, certificate)
     write_file(cafilepath, '')
+
 
 # if no config file present, setup config files
 if os.path.isfile(configfile):
@@ -44,18 +48,19 @@ else:
     print "created new config file"
     copy_default_config()
     config.read(configfile)
-    #gen uuid
+    # gen uuid
     uuid = random.randint(100000000000000L, 999999999999999L)
-    #set Path to Download Folder
+    # set Path to Download Folder
     default_download_dir = os.path.join(homedir, "Downloads")
     cfgfile = open(configfile, 'w')
-    config.set('general','uuid', uuid)
-    config.set('general','download_dir', default_download_dir)
+    config.set('general', 'uuid', uuid)
+    config.set('general', 'download_dir', default_download_dir)
     config.write(cfgfile)
     cfgfile.close()
-    #generate Keys and store in keydir
+    # generate Keys and store in keydir
     gen_and_store_keys(uuid)
     config.read(configfile)
+
 
 def load():
     global port, secure_port, bindip, uuid, downloaddir, auto_store_clipboard
@@ -71,35 +76,41 @@ def load():
     auto_open_urls = config.getboolean('permissions', 'auto_open_urls')
     auto_store_clipboard = config.getboolean('permissions', 'auto_store_clipboard')
 
+
 def get_port():
     return port
+
 
 def get_bindip():
     return bindip
 
+
 def get_download_dir():
-    return downloaddir+"/"
+    return downloaddir + "/"
+
 
 def write_config(datadict):
     if (not config):
         copy_default_config()
 
     cfgfile = open(configfile, 'w')
-    config.set('general','download_dir', datadict['downloaddir'])
-    config.set('general','debug', datadict['debug'])
-    config.set('network','bindip', datadict['bindip'])
-    config.set('network','port', datadict['port'])
-    config.set('network','secure_port', datadict['secure_port'])
-    config.set('permissions','auto_open_urls', datadict['auto_open_urls'])
-    config.set('permissions','auto_store_clipboard', datadict['auto_store_clipboard'])
-    config.set('permissions','auto_accept_files', datadict['auto_accept_files'])
+    config.set('general', 'download_dir', datadict['downloaddir'])
+    config.set('general', 'debug', datadict['debug'])
+    config.set('network', 'bindip', datadict['bindip'])
+    config.set('network', 'port', datadict['port'])
+    config.set('network', 'secure_port', datadict['secure_port'])
+    config.set('permissions', 'auto_open_urls', datadict['auto_open_urls'])
+    config.set('permissions', 'auto_store_clipboard', datadict['auto_store_clipboard'])
+    config.set('permissions', 'auto_accept_files', datadict['auto_accept_files'])
     config.write(cfgfile)
     cfgfile.close()
 
-#store server Process ID
+
+# store server Process ID
 def write_pidfile(pid):
     pfile = open(pidfile, 'w')
     pfile.write(pid)
     pfile.close()
+
 
 load()
--- server/deskcon-server.py
+++ server/deskcon-server.py
@@ -2,29 +2,31 @@
 # DesCon Desktop Server
 # Version 0.2
 
-import socket
-import webbrowser
-import signal
-import subprocess
-import platform
-import json
-import os
-import logging
-import notificationmanager
-import filetransfer
-import threading
-import thread
-import configmanager
-import mediacontrol
 import gi
-from OpenSSL import SSL
-from OpenSSL.SSL import ZeroReturnError
-from dbusservice import DbusThread
+import json
+import logging
+import os
+import platform
+import signal
+import socket
+import subprocess
+import thread
+import threading
+import webbrowser
 from base64 import b64decode
 
+from OpenSSL import SSL
+from OpenSSL.SSL import ZeroReturnError
+
+import configmanager
+import filetransfer
+import mediacontrol
+import notificationmanager
+from dbusservice import DbusThread
 from server.models.dataObject import DataObject
-from server.models.phone import Phone
+from server.models.phone import Phone, PhoneState
 from server.models.sessionInfo import SessionInfo
+from server.models.sms import Sms
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject, Gdk, GdkPixbuf
@@ -48,6 +50,7 @@ AUTO_ACCEPT_FILES = configmanager.auto_accept_files
 AUTO_OPEN_URLS = configmanager.auto_open_urls
 AUTO_STORE_CLIPBOARD = configmanager.auto_store_clipboard
 
+
 def reload_config():
     global HOST, SECUREPORT, AUTO_STORE_CLIPBOARD, AUTO_ACCEPT_FILES, AUTO_OPEN_URLS
     configmanager.load()
@@ -58,7 +61,7 @@ def reload_config():
     AUTO_STORE_CLIPBOARD = configmanager.auto_store_clipboard
 
 
-class Connector():
+class Connector:
     def __init__(self):
         signal.signal(signal.SIGINT, self.signal_handler)
         signal.signal(signal.SIGUSR1, self.signal_handler)
@@ -70,7 +73,7 @@ class Connector():
         self.dbus_service_thread = DbusThread(self)
         self.dbus_service_thread.daemon = True
 
-        self.sslserver = sslserver(self)
+        self.sslserver = SslServer(self)
         self.sslserver.daemon = True
 
     def run(self):
@@ -88,7 +91,7 @@ class Connector():
             # reload settings
             reload_config()
             # create new Thread
-            self.sslserver = sslserver(self)
+            self.sslserver = SslServer(self)
             self.sslserver.daemon = True
             self.sslserver.start()
             print "Server started"
@@ -108,93 +111,91 @@ class Connector():
         data_object = DataObject(**json.loads(data))
         print data_object.to_nice_string()
 
-        phone = \
-            Phone(data_object.uuid, data_object.device_name, None, None, False, 0, 0, address, False, 9096, None, None)
-
-        if [phone.uuid for phone in self.mid_info.phones if phone.uuid == data_object.uuid]:
-            self.mid_info.phones.append(phone)
-            print "created "+data_object.uuid
+        for phone in self.mid_info.phones:
+            if phone.uuid == data_object.uuid:
+                data_object.phone = phone
+                break
+        else:
+            data_object.phone = Phone(data_object.uuid, data_object.device_name, None, None, False, 0, 0,
+                                      address, False, 9096, None, None)
+            self.mid_info.phones.append(data_object.phone)
+            print "created " + data_object.uuid
 
         if data_object.data_type == "STATS":
-            phone.state = data_object.data
+            data_object.phone.state = PhoneState(**data_object.data)
 
-        elif (data_object.data_type == "SMS"):
-            smsobj = json.loads(data_object.data)
-            name = smsobj['name']
-            number = smsobj['number']
-            smsmess = smsobj['message']
+        elif data_object.data_type == "SMS":
+            sms = Sms(**data_object.data)
             notificationmanager.buildSMSReceivedNotification(
-                name, number, smsmess, address,
-                phone['controlport'], self.compose_sms)
+                sms.name, sms.number, sms.message, address,
+                data_object.phone.state.control_port, self.compose_sms)
 
-        elif (msgtype == "CALL"):
+        elif data_object.data_type == "CALL":
             notificationmanager.buildTransientNotification(
-                name, "incoming Call from " + message)
+                data_object.phone.device_name, "incoming Call from " + str(data_object.data))
 
-        elif (msgtype == "URL"):
+        elif data_object.data_type == "URL":
             if (AUTO_OPEN_URLS):
-                webbrowser.open(message, 0, True)
+                webbrowser.open(data_object.data, 0, True)
             else:
-                notificationmanager.buildNotification("URL", "Link: "+message)
+                notificationmanager.buildNotification("URL", "Link: " + data_object.data)
 
-        elif (msgtype == "CLPBRD"):
+        elif data_object.data_type == "CLPBRD":
             logger.debug("[Connector] CLPBRD received")
-            if message == None or len(message) == 0:
+            if data_object.data == None or len(data_object.data) == 0:
                 logger.debug("[Connector] CLPBRD received empty text, ignoring it")
                 return
 
             clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
             current_clipboard = clipboard.wait_for_text()
             logger.debug("[Connector] Current clipboard readed")
-            if current_clipboard == message:
+            if current_clipboard == data_object.data:
                 logger.info("[Connector] Not updating clipboard, no changes")
                 return
 
             if AUTO_STORE_CLIPBOARD:
                 logger.debug("[Connector] updating clipboard")
-                clipboard.set_text(message.encode("utf-8"), -1)
+                clipboard.set_text(data_object.data.encode("utf-8"), -1)
                 logger.debug("[Connector] clipboard updated")
-                notificationmanager.buildTransientNotification("Clipboard", message)
+                notificationmanager.buildTransientNotification("Clipboard", data_object.data)
             else:
                 # FIXME: no timeout / able to select text
-                notificationmanager.buildNotification("Clipboard", message)
+                notificationmanager.buildNotification("Clipboard", data_object.data)
 
-        elif (msgtype == "MIS_CALL"):
+        elif data_object.data_type == "MIS_CALL":
             notificationmanager.buildNotification(
-                name, "missed Call from " + message)
+                data_object.phone.device_name, "missed Call from " + str(data_object.data))
 
-        elif (data_object.data_type == "PING"):
+        elif data_object.data_type == "PING":
             notificationmanager.buildTransientNotification(
-                "Ping from " + data_object.device_name,
-                #"Name: " + name +
-                #"\nUUID: " + uuid +
-                "\nIP: " + address)
+                "Ping from " + data_object.phone.device_name,
+                "\nIP: " + data_object.phone.state.ip)
 
-        elif (msgtype == "OTH_NOT"):
-            msginfo = message.split(': ', 1)
-            new_notification = uuid+"::"+message
-            if (self.last_notification != new_notification):
+        elif data_object.data_type == "OTH_NOT":
+            msginfo = data_object.data.split(': ', 1)
+            new_notification = data_object.phone.uuid + "::" + data_object.data
+            if self.last_notification != new_notification:
                 self.last_notification = new_notification
                 self.dbus_service_thread.emit_new_notification_signal()
-                if (len(msginfo) > 1):
+                if len(msginfo) > 1:
                     sender = msginfo[0]
                     notification = msginfo[1]
                     notificationmanager.buildTransientNotification(
                         sender, notification)
                 else:
                     notificationmanager.buildTransientNotification(
-                        name, message)
+                        data_object.phone.device_name, data_object.data)
 
-        elif (msgtype == "FILE_UP"):
-            filenames = json.loads(message)
-            if (AUTO_ACCEPT_FILES):
+        elif data_object.data_type == "FILE_UP":
+            filenames = data_object.data
+            if AUTO_ACCEPT_FILES:
                 print "accepted"
                 filePaths = filetransfer.write_files(filenames, csocket)
                 notificationmanager.buildFileReceivedNotification(
                     filePaths, self.open_file)
             else:
                 accepted = notificationmanager.buildIncomingFileNotification(
-                    filenames, name)
+                    filenames, data_object.phone.device_name)
                 print "wait for ui"
                 if (accepted):
                     print "accepted"
@@ -204,16 +205,16 @@ class Connector():
                 else:
                     print "not accepted"
 
-        elif (msgtype == "MEDIACTRL"):
-            thread.start_new_thread(mediacontrol.control, (message,))
+        elif data_object.data_type == "MEDIACTRL":
+            thread.start_new_thread(mediacontrol.control, (data_object.data,))
 
-        elif (msgtype == "NOT_BIG"):
-            obj = json.loads(message)            
+        elif data_object.data == "NOT_BIG":
+            obj = json.loads(data_object.data)
             app = obj['appName']
             title = obj['title']
             text = obj['text']
-            new_notification = uuid+"::"+title+" - "+text
-            if (self.last_notification != new_notification):
+            new_notification = data_object.phone.uuid + "::" + title + " - " + text
+            if self.last_notification != new_notification:
                 self.last_notification = new_notification
                 self.dbus_service_thread.emit_new_notification_signal()
                 if "icon" in obj:
@@ -224,56 +225,52 @@ class Connector():
                     loader.close()
                 else:
                     pixbuf = None
-                            
+
                 notificationmanager.buildBigNotification(title, text, pixbuf)
                 print "NOT_BIG captured"
 
         else:
             print "ERROR: Non parsable Data received"
 
-        #print json.dumps(self.mid_info)
-
     def compose_sms(self, number, ip, port, msg):
-        subprocess.Popen([PROGRAMDIR+"/sms.py", ip, port, number, msg], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/sms.py", ip, port, number, msg], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def ping_device(self, ip, port):
-        subprocess.Popen([PROGRAMDIR+"/ping.py", ip, port], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/ping.py", ip, port], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def send_file(self, ip, port):
-        subprocess.Popen([PROGRAMDIR+"/filechooser.py", ip, port], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/filechooser.py", ip, port], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def set_clipboard(self, ip, port):
         print "setting clipboard"
-        subprocess.Popen([PROGRAMDIR+"/setClipboard.py", ip, port], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/setClipboard.py", ip, port], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def open_file(self, path):
-        if (path == ""):
+        if path == "":
             path = configmanager.downloaddir
         if platform.system() == "Windows":
             os.startfile(path)
         elif platform.system() == "Darwin":
             subprocess.Popen(["open", path], stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
             subprocess.Popen(["xdg-open", path], stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def show_settings(self):
-        subprocess.Popen([PROGRAMDIR+"/settingswindow.py"], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/settingswindow.py"], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def setup_device(self):
-        subprocess.Popen([PROGRAMDIR+"/setupdevice.py"], stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen([PROGRAMDIR + "/setupdevice.py"], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
-
-class sslserver(threading.Thread):
-
+class SslServer(threading.Thread):
     def __init__(self, conn):
         threading.Thread.__init__(self)
         self.running = True
@@ -284,18 +281,18 @@ class sslserver(threading.Thread):
         # Initialize context
         try:
             ctx = SSL.Context(SSL.SSLv23_METHOD)
-            ctx.set_options(SSL.OP_NO_SSLv2|SSL.OP_NO_SSLv3) #TLS1 and up
-            ctx.set_verify(SSL.VERIFY_PEER|SSL.VERIFY_FAIL_IF_NO_PEER_CERT, verify_cb) #Demand a certificate
+            ctx.set_options(SSL.OP_NO_SSLv2 | SSL.OP_NO_SSLv3)  # TLS1 and up
+            ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT, verify_cb)  # Demand a certificate
             ctx.set_cipher_list('HIGH:!SSLv2:!aNULL:!eNULL:!3DES:@STRENGTH')
             ctx.use_privatekey_file(configmanager.privatekeypath)
             ctx.use_certificate_file(configmanager.certificatepath)
             ctx.load_verify_locations(configmanager.cafilepath)
         except Exception as e:
             error = e[0]
-            if len(error)>0:  # ignore empty cafile error
+            if len(error) > 0:  # ignore empty cafile error
                 print error
         self.sslserversocket = SSL.Connection(ctx, socket.socket(socket.AF_INET,
-                             socket.SOCK_STREAM))
+                                                                 socket.SOCK_STREAM))
 
         self.sslserversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sslserversocket.bind(('', SECUREPORT))
@@ -325,7 +322,7 @@ class sslserver(threading.Thread):
                 self.conn.dbus_service_thread.emit_changed_signal()
             except Exception as e:
                 errnum = e[0]
-                if (errnum != 22):
+                if errnum != 22:
                     print "Error " + str(e[0])
             finally:
                 # close connection
@@ -343,9 +340,9 @@ class sslserver(threading.Thread):
 
 def verify_cb(conn, cert, errnum, depth, ok):
     # This obviously has to be updated
-    #print "er"+str(errnum)
-    #print "de"+str(depth)
-    #print "ok "+str(ok)
+    # print "er"+str(errnum)
+    # print "de"+str(depth)
+    # print "ok "+str(ok)
     return ok
 
 
@@ -354,9 +351,10 @@ def main():
     app = Connector()
     app.run()
 
+
 if __name__ == '__main__':
-   try:
-      main()
-   except KeyboardInterrupt:
-      print "\nServer stopped"
-      pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "\nServer stopped"
+        pass
